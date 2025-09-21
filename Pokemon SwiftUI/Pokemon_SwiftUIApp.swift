973515ce28fde11d3e780065777bd8d6486ac3cb
William Kindlien <william.k.gunawan@gmail.com>
2025-09-21 15:31:17 +0700

    Initial Commit

---
+++ Pokemon SwiftUI/Pokemon_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_SwiftUIApp.swift
+//  Pokemon SwiftUI
+//
+//  Created by William Kindlien Gunawan on 21/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
